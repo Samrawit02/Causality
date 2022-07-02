4810afb63717364dd07f85628582c5771a7d7245
Samrawit02 <hairaer02@gmail.com>
2022-07-02 22:27:34 +0300

    adding the models

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -22,8 +22,7 @@ def plot_correlation(x):
     mask[np.triu_indices_from(mask)] = True
     cmap = sns.diverging_palette(230, 20, as_cmap=True)
     fig, ax = plt.subplots(figsize=(24, 20))
-    heatmap = sns.heatmap(corr, mask=mask, square=True, linewidths=.5,
-                          vmin=-1, vmax=1, cmap='coolwarm', annot=True, fmt='.1f')
+    heatmap = sns.heatmap(corr, annot=True, linewidths=.5, fmt= '.1f',ax=ax)
     heatmap.set_title('Correlation between features',
                       fontdict={'fontsize': 15}, pad=12)
     fig.show()
